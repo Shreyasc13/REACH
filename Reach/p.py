ed2cf4a4e8cdf0229ba901ed95760be8c4a9871c
Shreyasc13 <44752893+Shreyasc13@users.noreply.github.com>
2021-01-27 23:57:05 +0530

    cleanup

--- Reach/p.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect('test.db')
-cur = con.cursor()
-
-cur.execute("insert into maintainer (username,password) values (?,?)", ('admin', 'password'))
-
-con.commit()
-con.close()
